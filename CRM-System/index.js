a73be8d6ab3fc502894a52872866061c15afe6bc
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-01-22 22:16:14 +0530

    cors configration

--- CRM-System/index.js
+++ CRM-System/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
